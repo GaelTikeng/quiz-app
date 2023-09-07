281c6b161846a682c9aab4fcfef5dffc6d504fe3
ricardo <ricardotsopse@gmail.com>
2023-09-07 10:19:41 +0100

    footer done succesfully

--- front-end/src/component/molecule/Footer.jsx
+++ front-end/src/component/molecule/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import InputField from "../atoms/InputFields /InputField";
 import Button from "../atoms/button/Button";
 import "./Footer.css";
 import Email from "../../../public/image/email.png";
@@ -9,18 +8,18 @@ function Footer() {
     <div className="footer">
       <div className="footerHeader">
         <h2>Start Your Free Trial</h2>
-        <p>No credit card required. Try out the app for free</p>
+        <p>No credit card required. Try out the app for free!</p>
       </div>
       <form className="footerForm">
         <div className="Inputfooter">
-          <img src={Email} alt="email_logo" />
+          <img src={Email} alt="email_logo" className="footerLogo" />
           <input
             type="text"
             placeholder="Please Enter your work email"
             className="footerInput"
           />
         </div>
-        <Button className="footerbtn" title="Start Trial" />
+        <Button className="footerbtn" title="START TRIAL" />
       </form>
     </div>
   );
