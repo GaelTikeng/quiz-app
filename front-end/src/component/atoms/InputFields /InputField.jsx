9743d22a549bd32c6c1ae949e286b95317ac6673
ricardo <ricardotsopse@gmail.com>
2023-09-07 10:54:18 +0100

    sign up form under construction

--- front-end/src/component/atoms/InputFields /InputField.jsx
+++ front-end/src/component/atoms/InputFields /InputField.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function InputField({text}) {
+function InputField({ type, value, name, placeholder, label, onChange }) {
   return (
-    <div>
-        <input type="text" placeholder=''/>
+    <div className="form-group">
+      {label && <label htmlFor="input-field">{label}</label>}
+      <input
+      className="form_control"
+       type={type} 
+       value={value} 
+       placeholder={placeholder} 
+       name={name} 
+       onChange={onChange}/>
     </div>
-  )
+  );
 }
 
-export default InputField
+export default InputField;
