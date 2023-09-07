0ade297f38d30d07f2fdc10448a66f7931b35fa9
GaelTikeng <Gaelinho@yahoo.fr>
2023-09-07 11:36:21 +0100

    sending email from backend part one

--- back-end/router/user.js
+++ back-end/router/user.js
@@ -3,13 +3,73 @@ const router = express.Router();
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 
+const nodemailer = require("nodemailer")
+
 const User = require("../models/user");
+const Mailgen = require("mailgen");
 
 router.post("/user/signup", async (req, res) => {
   let { username, email, password } = req.body;
   let hashedPassword;
 
+  // sendiing email
+  let config = {
+    service: 'gmail', // email domain
+    auth: {
+      user: process.env.MY_GMAIL,
+      pass: process.env.GMAIL_APP_PASSWORD
+    }
+  }
+
+  let transporter = nodemailer.createTransport(config)
+  let MailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Mailgen",
+      link: 'https://mailgen.js'
+    }
+  })
+
+  let response = {
+    body: {
+      name: "daily tuition",
+      into: "Welcome to Smartbrain",
+      table: {
+        data: [
+          {
+            item: "SmartBrain",
+            description: "An online quiz app",
+            price: "free"
+          }
+        ]
+      },
+      outro: "looking forward to work with you"
+    }
+    
+  }
+
+  let mail = MailGenerator.generate(response)
+  let message = {
+    from: process.env.MY_GMAIL,
+    to: email,
+    subject: "Welcome to SmartBrain",
+    text: "The quiz app that helps you create engaging and interactive quizzes that will asses your students knowledge understanding of your subject matter",
+    html: mail
+  }
+
   try {
+    console.log("sending email")
+    transporter.sendMail(message).then((info) => {
+      console.log('email send successfully')
+      return res.status(201).json({
+        msg: "sent",
+        info: info.messageId,
+        previeuw: nodemailer.getTestMessageUrl(info)
+      }).catch((err) => {
+        console.log("error occured while sending email", err)
+        return res.status(500).json({msg: err})
+      })
+    })
     bcrypt
       .hash(password, 10)
       .then((hash) => {
@@ -21,7 +81,7 @@ router.post("/user/signup", async (req, res) => {
     const alreadyExistUser = await User.findOne({ where: { email } }).catch(
       (err) => console.log("error getting user", err)
     );
-    if (alreadyExistUser) return res.send("User already exist with this same email");
+    // if (alreadyExistUser) return res.send("User already exist with this same email");
 
     await User.create({
       username: username,
