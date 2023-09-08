712f9ecfeceb9248374d09f823fb0e2c2dbb7033
ricardo <ricardotsopse@gmail.com>
2023-09-08 11:54:09 +0100

    Login page under construction

--- front-end/src/component/organism/UserLogin/UserLogin.jsx
+++ front-end/src/component/organism/UserLogin/UserLogin.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import './UserLogin.css';
-import Navbar from '../../molecule/Navbar';
-import InputField from '../../atoms/InputFields/InputField';
-import Button from '../../atoms/button/Button';
-import Logo from '../../../../public/image/Authentication-pana1.png'
+import React from "react";
+import "./UserLogin.css";
+import Navbar from "../../molecule/Navbar";
+import InputField from "../../atoms/InputFields/InputField";
+import Button from "../../atoms/button/Button";
+import Logo from "../../../../public/image/Authentication-pana1.png";
 
 function UserLogin() {
   return (
     <>
-       <Navbar/> 
-       <div className='userlogin_container'>
-        <form action="submit" className='uselogin_form'>
-          <h1>Login</h1>
-          <div className='userlogin_details'>
-            <InputField/>
-            <InputField/>
-            <p><span>Forgot Password?</span></p>
-            <Button title="Login"/>
+      <Navbar />
+      <div className="userlogin_container">
+        <form action="submit" className="uselogin_formcontainer">
+          <div className="userlogin_input">
+            <h1>Login</h1>
+            <div className="userlogin_details">
+              <InputField />
+              <InputField />
+              <p>
+                <span>Forgot Password?</span>
+              </p>
+              <Button title="Login" />
+            </div>
           </div>
-          <div className='userlogin_image'>
-            <img src={Logo} alt="userlogin_image" />
+          <div className="userlogin_image">
+            <img src={Logo} alt="userlogin_image" className="userlogin_image" />
           </div>
-          </form>
-       </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default UserLogin
+export default UserLogin;
