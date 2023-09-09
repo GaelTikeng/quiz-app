88ca429a9dd32d2e26ce6fec866c8cdf35445061
ricardo <ricardotsopse@gmail.com>
2023-09-09 06:35:05 +0100

    userOnboarding page under construction

--- front-end/src/component/organism/Onboarding1/UserOnboard.jsx
+++ front-end/src/component/organism/Onboarding1/UserOnboard.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import "./UserOnboard.css";
 import Logo from "../../../../public/image/smartbrain.jpg";
 import Logo1 from "../../../../public/image/Welcome-bro1.png";
-import Vector1 from '../../../../public/image/Vector1.png'
+import Vector1 from '../../../../public/image/Vector1.png';
+import Vector2 from '../../../../public/image/Vector 2.png'
+import { useNavigate } from "react-router-dom";
 
 function UserOnboard() {
+ const navigate = useNavigate();
+
+  const handleprevClick =() =>{
+    navigate(-1)
+  };
+
+  const handlenextClick =() => {
+    navigate(+1)
+  }
+
+
+
   return (
     <div className="useronboard_container">
       <div className="useronboard_log">
@@ -18,8 +32,8 @@ function UserOnboard() {
           teacher to help you learn the material.
         </p>
         <div className="navig_btns">
-          <p><img src={Vector1} alt="vector" />  prev</p>
-          <p><img src={Vector2} alt="vector" /> prev</p>
+          <p onClick={handleprevClick}><img src={Vector1} alt="vector" />  prev</p>
+          <p onClick={handlenextClick}><img src={Vector2} alt="vector" /> next</p>
         </div>
       </div>
     </div>
