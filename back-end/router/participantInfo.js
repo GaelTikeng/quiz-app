360c10a0fe569a22fa3259a0bf612ee8881b4ca0
GaelTikeng <Gaelinho@yahoo.fr>
2023-09-11 17:09:02 +0100

    added quizId on post request of participant info

--- back-end/router/participantInfo.js
+++ back-end/router/participantInfo.js
@@ -1,40 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const Participant = require('../models/participant')
+const Participant = require("../models/participant");
 
-
-router.post('/student', async (req, res) => {
-  const name = req.body['Full name']
-  const email = req.body['Email address']
+router.post("/student/:quizId", async (req, res) => {
+  const name = req.body["Full name"];
+  const email = req.body["Email address"];
+  const { quizId } = req.params();
 
   try {
-    console.log(name)
-    console.log('participant email', )
+    console.log(name);
+    console.log("participant email");
     await Participant.create({
       name: name,
-      email: email
-    })
+      email: email,
+      quizId: quizId,
+    });
 
     res.send({
       participantName: name,
-      participantEmail: email
-    })
+      participantEmail: email,
+    });
+  } catch (error) {
+    console.log("could not save student credentials", error);
   }
-  catch (error) {
-    console.log("could not save student credentials", error)
-  }
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
