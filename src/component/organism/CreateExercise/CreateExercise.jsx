710f1d8c2d8007e969ba3ac6f68a1d6db571394b
ricardo <ricardotsopse@gmail.com>
2023-09-15 14:41:47 +0100

    some functionalities added to the create exercise page

--- src/component/organism/CreateExercise/CreateExercise.jsx
+++ src/component/organism/CreateExercise/CreateExercise.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateExercise.css";
 import Usersnav from "../../molecule/Usersnav/Usersnav";
 import Sidebar from "../../molecule/Sidebar/Sidebar";
-// import InputField from "../../atoms/InputFields/InputField";
 import InputField from "../../atoms/InputFields/InputField";
 import { MdCancel } from "react-icons/md";
 import { MdOutlineAddCircle } from "react-icons/md";
 import Button from "../../atoms/button/Button";
+import { useNavigate } from "react-router-dom";
 
 function CreateExercise() {
+  const navigate = useNavigate();
+  const [quizTitle, setQuizTitle] = useState("");
+  const [question, setQuestion] = useState([]);
+
+  const [newOption,  setNewOption] = useState('');
+  const [options, setOptions] = useState([]);
+
+
+  //set quiz_title function  
+  function handleChange(e) {
+    setQuizTitle(e.target.value);
+  };
+  
+  //set question function 
+  function handlequestion(e) {
+    setQuestion(e.target.value);
+  };
+  // handles options deletion   
+  function handleoptions(e) {
+    setOptions(e.target.value);
+  };
+
+  const deleteOption = (index) => {
+    const updatedoption = [...options];
+    updatedoption.splice(index, 1);
+    setOptions(updatedoption)
+  }
+
+  const navtoprevpage = () => {
+    navigate("-1");
+  };
+
+  const navtonextpage = () => {
+    navigate("1");
+  };
+
   return (
     <>
       <Usersnav />
@@ -20,13 +56,24 @@ function CreateExercise() {
           </div>
           <div className="inputs_container">
             <div className="creat_quiz">
-              <InputField label="Quiz title" type="text" className="quiz_title"/>
+              <InputField
+                label="Quiz title"
+                type="text"
+                className="quiz_title"
+                value={quizTitle}
+                onChange={handleChange}
+              />
             </div>
             <div className="texterea">
-              <label htmlFor="question" className="textarea_label">
+              <label
+                htmlFor="question"
+                className="textarea_label"
+                value={question}
+                onChange={handlequestion}
+              >
                 Question
               </label>
-              <textarea name="" id="">
+              <textarea name="question" id="question">
                 enter the question...
               </textarea>
             </div>
@@ -44,24 +91,34 @@ function CreateExercise() {
                 <div className="answers">
                   <InputField
                     type="text"
-                    name="option"
+                    name="option"handleoptionshandleoptions
                     className="text_input"
+                    value={newOption}
+                    onChange={event => setNewOption(event.target.value)}
                   />
                 </div>
-                <MdCancel className="clear_btn" />
+                <MdCancel className="clear_btn" onClick={() => deleteOption(index)}/>
               </div>
             </div>
             <div className="adding">
               <div className="add_btn">
-                <MdOutlineAddCircle className="md_add"/> Add option
+                <MdOutlineAddCircle className="md_add" /> Add option
               </div>
             </div>
             <div className="buttom_btn">
               <div className="buttons">
                 <Button title="cancel" className="cancel" />
                 <div className="three_btn">
-                  <Button title="Prev" className="previous" />
-                  <Button title="Next" className="next_btn" />
+                  <Button
+                    title="Prev"
+                    onClick={navtoprevpage}
+                    className="previous"
+                  />
+                  <Button
+                    title="Next"
+                    onClick={navtonextpage}
+                    className="next_btn"
+                  />
                   <Button title="Done" className="done_btn" />
                 </div>
               </div>
