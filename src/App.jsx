3e5c8a36561dcc6f86c22d58003b4d1abd6d06e8
ricardo <ricardotsopse@gmail.com>
2023-09-15 04:08:05 +0100

    some changes done in the students score page

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Landing from './component/organism/Landing'
+import SignUp from './component/organism/Sign_up/SignUp'
+import UserLogin from './component/organism/UserLogin/UserLogin'
+import ParticipantsLogin from './component/organism/ParticipantsLogin/ParticipantsLogin'
+import UserDashboard from './component/organism/UserDashboard/UserDashboard'
+import CreateExercise from './component/organism/CreateExercise/CreateExercise'
+import Studscore from './component/organism/Studscore/Studscore'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <Router>
+
+         <Routes>
+             <Route path='/' element={<Landing/>}/>
+             <Route path='/account/signup' element={<SignUp/>}/>
+             <Route path='/account/login' element={<UserLogin/>}/>
+             <Route path='/welcome' element={<ParticipantsLogin/>}/>
+             <Route path='/dashboard:/userid' element={<UserDashboard/>}/>
+             <Route path='/dashboard/createexercise' element={<CreateExercise/>}/>
+             <Route path='/student/score' element={<Studscore/>}/>
+           </Routes>
+    </Router>
+
+    </div>    
+  )
+} 
+
+export default App
