ce640f945ba0e6134d78b3168ecad673694d06d9
ricardo <ricardotsopse@gmail.com>
2023-09-18 14:52:01 +0100

    boiler plate for my page done

--- front-end/src/pages/Useronboardinga/Useronboardinga.jsx
+++ front-end/src/pages/Useronboardinga/Useronboardinga.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import "./Useronboardinga.css";
+import Usersnav from "../../component/molecule/Usersnav/Usersnav";
+import Logo from "../../../public/image/useronboard1.png";
+import { GrFormPrevious } from "react-icons/gr";
+import { GrFormNext } from "react-icons/gr";
+
 
 function Useronboardinga() {
   return (
-    <div className="useronboarding_container">
-      <div></div>
-    </div>
+    <>
+      <Usersnav />
+      <div className="useronboard_container">
+        <div className="div_elements">
+          <div className="header">
+            <h1>You are in</h1>
+          </div>
+          <div className="useronboarding_img">
+            <img src={Logo} alt="img" />
+          </div>
+          <div>
+            <p>
+              Are you ready to test your knowledge? This quiz app was <br />{" "}
+              created by your teacher to help you learn the material.
+            </p>
+          </div>
+          <div className="btns">
+            <p className="prev_navig"><GrFormPrevious/> Prev</p>
+            <p>1/2</p>
+            <p className="next_navig"><GrFormNext/> Next</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
