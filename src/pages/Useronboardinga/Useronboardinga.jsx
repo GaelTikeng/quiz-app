7aa294fd824bae895959a9e6b4adc5bd3750dc7a
ricardo <ricardotsopse@gmail.com>
2023-09-19 05:00:54 +0100

    ui done completely

--- src/pages/Useronboardinga/Useronboardinga.jsx
+++ src/pages/Useronboardinga/Useronboardinga.jsx
@@ -1,36 +1,47 @@
 import React from "react";
 import "./Useronboardinga.css";
-import Logo from "../../../public/image/useronboard1.png";
+import Logo from "../../../public/image/smartbrain.jpg";
+import Welcome from "../../../public/image/useronboard1.png";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
+import { useNavigate } from "react-router-dom";
 
 function Useronboardinga() {
+  const navigate = useNavigate();
+  const handleClickNext = () => {
+    navigate("/student/2");
+  };
+
   return (
     <div className="all">
       <div className="nav-img">
         <img src={Logo} alt="quiz_app logo" />
       </div>
-      <div className="grey-section">
-        <div className="welcome">
-          <h2>You are almost there !</h2>
-          {/* <h1>Welcome to SmartBrain</h1> */}
-          <img src={Welcome} alt="welcome image" className="image" />
-          <p className="p-tag">
-            Are you ready to test your knowledge? This quiz was created by your
-            teacher to help you learn the material.
-          </p>
-          <div className="two-btns">
-            <button
-              // onClick={handleClickPrev}
-              className="prev-btns"
-            >
-              <FaAngleLeft />
-              Prev
-            </button>
-            <p>1/2</p>
-            <button onClick={handleClickNext} className="next-btn">
-              Next
-              <FaAngleRight />
-            </button>
+      <div className="grey-sec">
+        <div className="grey-section">
+          <div className="welcome">
+            <div className="elt-holder">
+            <h2>You are almost there !</h2>
+            {/* <h1>Welcome to SmartBrain</h1> */}
+            <img src={Welcome} alt="welcome image" className="image" />
+            <p className="p-tag">
+              Manage your quizzes, users and results from here
+            </p>
+            <div className="two-btns">
+              <button
+                // onClick={handleClickPrev}
+                className="prev-btns"
+              >
+                <FaAngleLeft />
+                Prev
+              </button>
+              <p>1/2</p>
+              <button onClick={handleClickNext} className="next-btn">
+                Next
+                <FaAngleRight />
+              </button>
+            </div>
+          </div>
           </div>
         </div>
       </div>
