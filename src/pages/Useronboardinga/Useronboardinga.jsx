06ff977bd0429828cfd470529c23a90b9ce18749
ricardo <ricardotsopse@gmail.com>
2023-09-19 02:50:40 +0100

    some changes made

--- src/pages/Useronboardinga/Useronboardinga.jsx
+++ src/pages/Useronboardinga/Useronboardinga.jsx
@@ -1,43 +1,40 @@
 import React from "react";
 import "./Useronboardinga.css";
-import Usersnav from "../../component/molecule/Usersnav/Usersnav";
 import Logo from "../../../public/image/useronboard1.png";
-import { GrFormPrevious } from "react-icons/gr";
-import { GrFormNext } from "react-icons/gr";
+
 
 function Useronboardinga() {
   return (
-    <>
-      <Usersnav />
-      <div className="useronboard_container">
-        <div className="div_elements">
-          <div className="header_icon">
-            <div className="welcome_header">
-              <h1>You are in</h1>
-            </div>
-            <div className="useronboarding_img">
-              <img src={Logo} alt="img" />
-            </div>
-            <div className="welcome_text">
-              <p>
-                Are you ready to test your knowledge? This quiz app was <br />
-                created by your teacher to help you learn the material.
-              </p>
-            </div>
-
-            <div className="btns">
-              <p className="prev_navig">
-                <GrFormPrevious className="prev_icon" /> Prev
-              </p>
-              <p>1/2</p>
-              <p className="next_navig">
-                Next <GrFormNext className="next_icon" />
-              </p>
-            </div>
+    <div className="all">
+      <div className="nav-img">
+        <img src={Logo} alt="quiz_app logo" />
+      </div>
+      <div className="grey-section">
+        <div className="welcome">
+          <h2>You are almost there !</h2>
+          {/* <h1>Welcome to SmartBrain</h1> */}
+          <img src={Welcome} alt="welcome image" className="image" />
+          <p className="p-tag">
+            Are you ready to test your knowledge? This quiz was created by your
+            teacher to help you learn the material.
+          </p>
+          <div className="two-btns">
+            <button
+              // onClick={handleClickPrev}
+              className="prev-btns"
+            >
+              <FaAngleLeft />
+              Prev
+            </button>
+            <p>1/2</p>
+            <button onClick={handleClickNext} className="next-btn">
+              Next
+              <FaAngleRight />
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
