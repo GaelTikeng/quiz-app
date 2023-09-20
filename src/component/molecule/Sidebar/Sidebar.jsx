1a3595ffadd273e7714e2b10f39db94e5cd64ba9
ricardo <ricardotsopse@gmail.com>
2023-09-20 09:21:09 +0100

    modified the quiz and score buttons on the side bar

--- src/component/molecule/Sidebar/Sidebar.jsx
+++ src/component/molecule/Sidebar/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
-import quiz from "../../../../public/image/quiz_button.png";
-import Scores from "../../../../public/image/scores_button.png";
-import Logout from "../../../../public/image/logout_button.png";
+import quiz from "../../../../public/image/Group.png";
+import Logout from "../../../../public/image/Group1.png";
 import Avatar from 'react-avatar';
+import { GrScorecard } from "react-icons/gr"
 import { useNavigate } from "react-router-dom";
 
 function Sidebar() {
@@ -31,30 +31,29 @@ function Sidebar() {
         <div className="avatar_infos">
           <Avatar
             className="avatar"
-            name= {currentUser.username}
+            // name= {currentUser.username}
             color="#D9D9D9"
             round={true}
           />
-          <p>{currentUser.username}</p>
+          {/* <p>{currentUser.username}</p> */}
         </div>
       </div>
       <div className="dashboard_btn">
         <div className="side_midle">
-          <img
+         <p className="p1"><img
             src={quiz}
             alt="quiz_button"
             onClick={handlequiz}
             className="quiz_btn"
-          />
-          <img
-            src={Scores}
-            alt="score_btn"
+          /> Quiz</p>
+
+          <p className="p2"><GrScorecard
             onClick={handlescores}
             className="scores_btn"
-          />
+          />  Score</p>
         </div>
         <div className="logout">
-          <img src={Logout} alt="logout_btn" onClick={handlelogout} />
+          <p className="p3"><img src={Logout} alt="logout_btn" onClick={handlelogout} />LogOut</p>
         </div>
       </div>
     </div>
