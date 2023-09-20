b188cd29a35a975999c21ac099d058cb54da930a
ricardo <ricardotsopse@gmail.com>
2023-09-20 16:41:07 +0100

    some functionalities added in the taking quiz component

--- src/pages/Takingquiz/Takingquiz.jsx
+++ src/pages/Takingquiz/Takingquiz.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Usersnav from "../../component/molecule/Usersnav/Usersnav";
 import "./Takingquiz.css";
 import Button from "../../component/atoms/button/Button";
 
 function Takingquiz() {
+  // Declaring the state to store the quiz data
+  const [quizData, setQuizData] = useState([]);
+
+  // Function to fetch data from the api
+  const fetchquizdata = async () => {
+    const response = await fetch(" ");
+    const data = await response.json();
+    return data;
+  };
+
+  // Use the useEffect hook to fetch the quiz data when the component mounts.
+  useEffect =
+    (() => {
+      fetchquizdata().then((data) => {
+        setQuizData(data);
+      });
+    },
+    []);
+
   return (
     <>
       <Usersnav />
@@ -13,7 +32,20 @@ function Takingquiz() {
             <p>Name :</p>
             <p>Time left :</p>
           </div>
-          <div className="div_ques"></div>
+          <div className="div_ques">
+            <ol>
+              {quizData.map((question, index) => (
+                <li key={index}>
+                  <h2>{question.title}</h2>
+                  <ul>
+                    {question.options.map((option, index) => (
+                      <li key={index}>{option}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ol>
+          </div>
           <div className="btn_done">
             <Button title="Done" className="quiz-button" />
           </div>
