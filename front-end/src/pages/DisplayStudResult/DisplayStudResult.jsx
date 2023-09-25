231036b0f675d6896d9f7c5834bc42fef385d5a6
ricardo <ricardotsopse@gmail.com>
2023-09-25 14:11:44 +0100

    some changes made

--- front-end/src/pages/DisplayStudResult/DisplayStudResult.jsx
+++ front-end/src/pages/DisplayStudResult/DisplayStudResult.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import './DisplayStudResult.css'
-import Usersnav from '../../component/molecule/Usersnav/Usersnav'
-import Button from '../../component/atoms/button/Button'
+import React from "react";
+import "./DisplayStudResult.css";
+import Usersnav from "../../component/molecule/Usersnav/Usersnav";
+import Button from "../../component/atoms/button/Button";
 
-function DisplayStudResult() {
+const DisplayStudResult = ({studentName, score, quizTitle, timeSpent}) => {
   return (
     <div>
-        <Usersnav/>
-        <div className='result-cont'>
-           <div className='result-div'>
-               <h1 className='stud-name'>
-                Student name 
-               </h1>
-               <div className='score-details'>
-                <p>Quiz title</p>
-                <p>Score</p>
-                <p>Time spent</p>
-               </div>
-               <div className='action-btn'>
-                <Button title="Restart" className="restart-btn"/>
-                <Button title="Exit" className="exit-btn"/>
-               </div>
-           </div>
+      <Usersnav />
+      <div className="result-cont">
+        <div className="result-div">
+          <h1 className="stud-name">Student name: {studentName}</h1>
+          <div className="score-details">
+            <p>Quiz title : {score}</p>
+            <p>Score : {quizTitle}</p>
+            <p>Time spent : {timeSpent}</p>
+          </div>
+          <div className="action-btn">
+            <Button title="Restart" className="restart-btn" />
+            <Button title="Exit" className="exit-btn" />
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DisplayStudResult
+export default DisplayStudResult;
